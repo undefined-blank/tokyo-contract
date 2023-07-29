786888f0a58311ddb947d8e2c20598ae7d2aa7d3
Duhoon <eclip6@naver.com>
2023-07-29 14:53:43 +0900

    feat: Sign Verifier

--- contracts/Bet.sol
+++ contracts/Bet.sol
@@ -2,50 +2,71 @@
 pragma solidity ^0.8.9;
 
 import '@openzeppelin/contracts/utils/Counters.sol';
+import './utils/Signer.sol';
 
 contract Bet {
     using Counters for Counters.Counter;
 
     struct Player{
         address addr;
-        uint256 amount;
+        uint amount;
     }
 
-    struct Round{
+    struct Game{
         Player proposer;
-        Player opponent;
+        Player receiver;
         address withness;
+        uint amount;
         Status status;
     }
 
     enum Status {
+        Creating,
         Gaming,
-        Tying,
         Ended
     }
 
     event Proposal(address, address, uint256);
     event Result(address winner, uint256 round);
 
-    mapping(address => uint256) private nonce;
-    mapping(uint256 => Round) private rounds;
-    Counters.Counter private current_round;
+    mapping(uint256 => Game) private games;
+    Counters.Counter private current_game_id;
 
     constructor(){}
 
-    function get_nonce(address _address) public view returns (uint){
-        return nonce[_address];
+    function get_current_game() public view returns (uint){
+        return current_game_id.current();
     }
 
-    function get_current_round() public view returns (uint){
-        return current_round.current();
+    function create_game(address receiver, address withness, uint amount) public payable returns(bool){
+        require(msg.value >= amount, "Game: Transfer value should more than amount");
+
+        Game memory new_game = Game(
+            Player(
+                msg.sender,
+                msg.value
+            ),
+            Player(
+                receiver,
+                0
+            ), 
+            withness,
+            amount,
+            Status.Creating
+        );
+
+        current_game_id.increment();
+        games[current_game_id.current()] = new_game;
     }
 
-    function create_round(Round calldata _round) public returns(bool){
-        
+    function join_game(uint game_id) public payable returns(bool){
+        Game storage _game = games[game_id];
+        require(msg.value >= _game.amount, "Game: Transfer value should more than amount");
+        _game.receiver.amount = msg.value;
+    }
 
-        uint current = current_round.current();
-        rounds[current] = _round;
-        return true;
+    function verify(uint game_id, address winner,
+        bytes32 signature){
+            
     }
 }
---
+++ contracts/utils/Signer.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import '../Bet.sol';
+
+contract Signer {
+    struct Message{
+        address proposer;
+        address receiver;
+        address withness;
+        uint amount;
+    }
+    
+    struct Judge{
+        uint256 game_id;
+        address winner;
+    }
+
+    bytes32 constant PERMIT_TYPEHASH = keccak256(
+        "Permit(address proposer,address receiver,address withness,unit256 amount)"
+    );
+
+    bytes32 constant JUDGE_TYPEHASH = keccak256(
+        "Judge(uint256 game_id,address winner)"
+    );
+    
+    bytes32 public DOMAIN_SEPARATOR;
+
+    constructor(){
+        DOMAIN_SEPARATOR = keccak256(
+            abi.encode(
+                keccak256('BETDomain(string name,string version,unit256 chainId,address verifyingContract)'),
+                keccak256(bytes('Bet')),
+                keccak256(bytes('1.0')),
+                81,
+                address(this)
+            )
+        );
+    }
+    
+    function verify_proposer(
+        address proposer, address receiver, address withness, uint256 amount,
+        uint8 v, bytes32 r, bytes32 s
+    ) public pure returns (bool){
+        bytes32 hashedMessage = hashStruct_message(Message(proposer, receiver, withness, amount));
+
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                '\x19\x01',
+                DOMAIN_SEPARATOR,
+                hashedMessage
+            )
+        );
+
+        address recoveredAddress = ecrecover(digest, v, r, s);
+        require(recoveredAddress == proposer, "Verify: Fail");
+
+        return true;
+    }
+
+    function verify_withness(
+        uint256 game_id, address winner,
+        uint8 v, bytes32 r, bytes32 s
+    ) public view returns(bool){
+    }
+    
+    function hashStruct_message(Message memory _message) pure public returns (bytes32 hash){    
+        return keccak256(abi.encode(
+            PERMIT_TYPEHASH,
+            _message.proposer,
+            _message.receiver,
+            _message.withness,
+            _message.amount
+        ));
+    }   
+
+    function hashStruct_judge(Judge memory _judge) pure public returns (bytes32 hash){
+        return keccak256(abi.encode(
+            
+        ))
+    }
+
+}
